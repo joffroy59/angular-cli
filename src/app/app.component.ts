bb183c478f64ba63bcbfb5934287316f49c7b394
Jérôme Offroy <jerome.offroy@gmail.com>
2023-04-19 21:16:11 +0000

    chore(primeng): progressbar with value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Angular CLI Template";
+  value: number = 0;
+
+  ngOnInit() {
+    let interval = setInterval(() => {
+      this.value = this.value + Math.floor(Math.random() * 10) + 1;
+      if (this.value >= 100) {
+        this.value = 0;
+      }
+    }, 2000);
+  }
 }
