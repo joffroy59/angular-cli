07ce9c319fdb854d65a1000ed942f4500c7d0e2b
Jérôme Offroy <jerome.offroy@gmail.com>
2023-04-20 19:14:41 +0000

    refact: rename component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "./shared/shared.module";
 
 import { AppComponent } from "./app.component";
-import { ProgressBarDemoComponent } from "./progress-bar-demo/progress-bar-demo.component";
+import { PrimengProgressBarComponent } from "./primeng-progress-bar/primeng-progress-bar.component";
 import { DemoNgflexComponent } from "./demo-ngflex/demo-ngflex.component";
 import { DemoPrimengButtonComponent } from "./demo-primeng-button/demo-primeng-button.component";
 import { DemoPrimengProgressBarComponent } from "./demo-primeng-progress-bar/demo-primeng-progress-bar.component";
@@ -11,7 +11,7 @@ import { DemoPrimengProgressBarComponent } from "./demo-primeng-progress-bar/dem
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressBarDemoComponent,
+    PrimengProgressBarComponent,
     DemoNgflexComponent,
     DemoPrimengButtonComponent,
     DemoPrimengProgressBarComponent,
--- src/app/primeng-progress-bar/primeng-progress-bar.component.ts
+++ src/app/primeng-progress-bar/primeng-progress-bar.component.ts
@@ -2,18 +2,17 @@ import { Component, OnInit, Input } from "@angular/core";
 import { ProgressService } from "../services/progress.service";
 
 @Component({
-  selector: "app-progress-bar-demo",
-  templateUrl: "./progress-bar-demo.component.html",
-  styleUrls: ["./progress-bar-demo.component.css"],
+  selector: "app-primeng-progress-bar",
+  templateUrl: "./primeng-progress-bar.component.html",
+  styleUrls: ["./primeng-progress-bar.component.css"],
 })
-export class ProgressBarDemoComponent implements OnInit {
+export class PrimengProgressBarComponent implements OnInit {
   constructor(private progressService: ProgressService) {}
 
   @Input() title1: string;
   @Input() title2: string;
   @Input() title3: string;
 
-  title = "Angular CLI Template";
   value: number = 0;
 
   getProgress(): number {
