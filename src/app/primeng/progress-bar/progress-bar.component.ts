136decfd818abba0d3333168f94e70e9255011f1
Jérôme Offroy <jerome.offroy@gmail.com>
2023-04-20 20:03:39 +0000

    log progressbar

--- src/app/primeng/progress-bar/progress-bar.component.ts
+++ src/app/primeng/progress-bar/progress-bar.component.ts
@@ -9,9 +9,9 @@ import { ProgressService } from "../../services/progress.service";
 export class PrimengProgressBarComponent implements OnInit {
   constructor(private progressService: ProgressService) {}
 
-  @Input() title1: string;
-  @Input() title2: string;
-  @Input() title3: string;
+  @Input() title1: string = "";
+  @Input() title2: string = "";
+  @Input() title3: string = "";
 
   value: number = 0;
 
@@ -19,9 +19,18 @@ export class PrimengProgressBarComponent implements OnInit {
     return this.progressService.getProgress();
   }
 
+  toString(): string {
+    let res = "";
+    if (this.title1 != "") res += this.title1 + "-";
+    if (this.title2 != "") res += this.title2 + "-";
+    if (this.title3 != "") res += this.title3 + "-";
+    return "[" + res.slice(0, -1) + "]";
+  }
+
   ngOnInit() {
     let interval = setInterval(() => {
       this.value = this.value + this.getProgress() + 1;
+      console.log(this.toString() + "=" + this.value);
       if (this.value >= 100) {
         this.value = 0;
       }
