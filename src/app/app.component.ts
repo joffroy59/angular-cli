c222a60408c516b65c7a68108f63f1f355d27304
Jérôme Offroy <jerome.offroy@gmail.com>
2023-04-20 17:08:45 +0000

    use only component for value progressbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { ProgressService } from "./services/progress.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor(private progressService: ProgressService) {}
-
+export class AppComponent {
   title = "Angular CLI Template";
-  value1: number = 0;
-  value2: number = 0;
-
-  getProgress(): number {
-    return this.progressService.getProgress();
-  }
-
-  ngOnInit() {
-    let interval = setInterval(() => {
-      this.value1 = this.value1 + Math.floor(Math.random() * 10) + 1;
-      this.value2 = this.value2 + this.getProgress() + 1;
-      if (this.value1 >= 100) {
-        this.value1 = 0;
-      }
-      if (this.value2 >= 100) {
-        this.value2 = 0;
-      }
-    }, 2000);
-  }
 }
