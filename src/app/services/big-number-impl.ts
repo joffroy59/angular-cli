c91ebb7e7c62db147da10c7fe6067ca1206da855
Jérôme Offroy <jerome.offroy@gmail.com>
2023-04-21 20:46:57 +0000

    2

--- src/app/services/big-number-impl.ts
+++ src/app/services/big-number-impl.ts
@@ -1,1 +1,43 @@
-export class BigNumberImpl {}
+import { Info } from "./info";
+
+export class BigNumberImpl {
+  //_bigNumberStart: number = 6791;
+  _bigNumberStart: number = 1200;
+
+  getNextCounter(info: Info, delta: number): number {
+    this.log(info);
+    console.log("delta:" + delta);
+
+    let result = Math.floor(
+      (1 - Math.min(info.deltaSum + delta, 100) / 100) * this._bigNumberStart
+    );
+    console.log("result=" + result);
+
+    return result;
+  }
+
+  getResetCounter(): number {
+    return this._bigNumberStart;
+  }
+
+  next(info: Info): Info {
+    console.log("impl_bigNumber");
+    let delta = Math.floor(Math.random() * 30) + 1;
+    let deltaSumOld = info.deltaSum;
+
+    let counter = this.getNextCounter(info, delta);
+    return {
+      value: delta,
+      info: delta.toString(),
+      counter: counter,
+      deltaSum: deltaSumOld + delta,
+    };
+  }
+
+  log(info: Info) {
+    console.log("val=" + info.value);
+    console.log("info=" + info.info);
+    console.log("counter=" + info.counter);
+    console.log("deltaSum=" + info.deltaSum);
+  }
+}
--- src/app/services/progress.service.ts
+++ src/app/services/progress.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Info } from "./info";
+import { BigNumberImpl } from "./big-number-impl";
 
 @Injectable({
   providedIn: "root",
@@ -101,48 +102,6 @@ export class ProgressService {
 
   impl_bigNumber(): void {
     this.info = this.bigNumberImpl.next(this.info);
-  }
-
-  log(info: Info) {
-    console.log("val=" + info.value);
-    console.log("info=" + info.info);
-    console.log("counter=" + info.counter);
-    console.log("deltaSum=" + info.deltaSum);
-  }
-}
-
-export class BigNumberImpl {
-  //_bigNumberStart: number = 6791;
-  _bigNumberStart: number = 1200;
-
-  getNextCounter(info: Info, delta: number): number {
-    this.log(info);
-    console.log("delta:" + delta);
-
-    let result = Math.floor(
-      (1 - Math.min(info.deltaSum + delta, 100) / 100) * this._bigNumberStart
-    );
-    console.log("result=" + result);
-
-    return result;
-  }
-
-  getResetCounter(): number {
-    return this._bigNumberStart;
-  }
-
-  next(info: Info): Info {
-    console.log("impl_bigNumber");
-    let delta = Math.floor(Math.random() * 30) + 1;
-    let deltaSumOld = info.deltaSum;
-
-    let counter = this.getNextCounter(info, delta);
-    return {
-      value: delta,
-      info: delta.toString(),
-      counter: counter,
-      deltaSum: deltaSumOld + delta,
-    };
   }
 
   log(info: Info) {
